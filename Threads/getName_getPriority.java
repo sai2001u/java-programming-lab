878d28cb752f5b4ed6ad6e24ba5b46079885954a
sai2001u <69143912+sai2001u@users.noreply.github.com>
2021-07-13 14:18:06 +0530

    Create getName_getPriority.java

---
+++ Threads/getName_getPriority.java
@@ -0,0 +1,8 @@
+class getName_getPriority
+{
+public static void main (String args[])
+{
+System.out.println("Thread Name = " + Thread.currentThread().getName());
+System.out.println(" Priority = " + Thread.currentThread().getPriority());
+}
+} 
